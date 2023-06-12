73e4805dfe6f2fe3077952e576b8e43fb4a5ab54
10thcode <attabadukwabena@gmail.com>
2023-06-12 13:29:20 +0000

    Update 3-prin...: Not using .reverse() function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for item in my_list:
+    for item in range(len(my_list), 0, -1):
         print("{:d}".format(item))
