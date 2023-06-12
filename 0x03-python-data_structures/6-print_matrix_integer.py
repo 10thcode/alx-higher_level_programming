1734ed9db6e36aef1da519863a874159279690a2
10thcode <attabadukwabena@gmail.com>
2023-06-12 19:13:30 +0000

    Update 6-print_...: printing new line after loop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for li in matrix:
         for item in li:
             if li.index(item) == len(li) - 1:
-                print("{:d}".format(item))
+                print("{:d}".format(item), end="")
             else:
                 print("{:d}".format(item), end=" ")
+        print()
