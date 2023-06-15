d6e59bd04efaeccee5bf2e331c0469e63d21a3d3
10thcode <attabadukwabena@gmail.com>
2023-06-15 02:10:43 +0000

    Update 10-best...py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
-
-    key = 0
+    key = None
     highest = 0
-
-    for item in a_dictionary.values():
-        highest = item
-        break;
     
-    for k, v in a_dictionary.items():
-        if v > highest:
-            key = k
-            highest = v
+    if a_dictionary:
+        for k, v in a_dictionary.items():
+            if v > highest:
+                key = k
+                highest = v
 
     return key
