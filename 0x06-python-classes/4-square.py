bb78e1c17c8e0a38d13dc18557018ced262a4755
10thcode <attabadukwabena@gmail.com>
2023-06-28 01:10:39 +0000

    Add 4-square.py: Add getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Utitility for a square """
+
+
+class Square:
+    """ Blueprint for a square """
+    def __init__(self, size=0):
+        """
+        Construct a square
+
+        Args:
+            size (int): size of a square
+        """
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        """ Retrieve __size attribute """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Retrieve __size attribute
+
+        Args:
+            value (int): the value to assign to __size
+        """
+        if type(value) == int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+        Calculates the area of a square
+
+        Return:
+            int: Area of a square
+        """
+        return self.__size ** 2
