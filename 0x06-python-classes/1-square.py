5a69da35a262203262e31963bda9b8d5d5e7ec06
10thcode <attabadukwabena@gmail.com>
2023-06-28 00:41:57 +0000

    Add 1-square.py: define a class with a constructor

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Utitility for a square """
+
+
+class Square:
+    """ Blueprint for a square """
+    def __init__(self, size):
+        """
+        Construct a square
+
+        Args:
+            size (int): size of a square
+        """
+        self.__size = size
