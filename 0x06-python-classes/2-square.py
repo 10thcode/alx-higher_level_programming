53f1dd01fb48b1baf4b2344dc1a46d8f39d31c48
10thcode <attabadukwabena@gmail.com>
2023-06-28 00:51:11 +0000

    Add 2-square.py: Validate size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Utitility for a square """
+
+
+class Square:
+    """ Blueprint for a square """
+    def __init__(self, size=0):
+        """
+        Construct a square
+
+        Args:
+            size (int): size of a square
+        """
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
