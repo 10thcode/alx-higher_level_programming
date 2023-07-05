3070b66e5f0fe924307a1bfba8c695fda0b34849
10thcode <attabadukwabena@gmail.com>
2023-07-05 02:41:01 +0000

    Add 5-text_indentation.py and tests/5-text_indentation.txt

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module define a function that prints a text with 2 new lines
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    A function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    skip_space = True
+    for letter in text:
+        if letter == " " and skip_space:
+            continue
+        if letter not in [".", "?", ":"]:
+            print("{}".format(letter), end="")
+            skip_space = False
+        else:
+            print("{}".format(letter), end="\n\n")
+            skip_space = True
