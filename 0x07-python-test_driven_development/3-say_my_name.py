85513698ec9649861e6ce9504419a15474d573f6
10thcode <attabadukwabena@gmail.com>
2023-07-06 00:18:16 +0000

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,7 +6,7 @@ My name is Black Genius
 """
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(first_name="", last_name=""):
     """
     A function that prints My name is <first name> <last name>
     """
@@ -14,4 +14,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
+    if first_name == "" and last_name == "":
+        raise ValueError("no name was provided")
     print("My name is {} {}".format(first_name, last_name))
