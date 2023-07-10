71c99eb051c006463a1224cef0dac5a5fa4e8392
10thcode <attabadukwabena@gmail.com>
2023-07-10 20:35:22 +0000

    Add 6-base_geometry.py: Define a function that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines an empty class BaseGeometry. """
+
+
+class BaseGeometry():
+    """ Geometry class """
+    def area(self):
+        """ Raises an exception """
+        raise Exception("area() is not implemented")
