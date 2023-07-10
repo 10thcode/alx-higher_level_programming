5410b39f703490749e970c380f93ca60cf97a00e
10thcode <attabadukwabena@gmail.com>
2023-07-10 19:11:36 +0000

    Add 1-my_list.py: define a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+"""
+This module defines a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """ a class that inherits from list """
+    def print_sorted(self):
+        """ prints the list, but sorted (ascending sort) """
+        print(sorted(super().copy()))
