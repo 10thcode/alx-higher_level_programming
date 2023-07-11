01f20e2ae5d01a77ff0888c981302bfea90c3d25
10thcode <attabadukwabena@gmail.com>
2023-07-11 04:44:51 +0000

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a a function that adds
+a new attribute to an object if it’s possible
+"""
+
+
+def add_attribute(obj, key, value):
+    if type(obj).__name__ not in dir(__import__("builtins")):
+        if "__slots__" not in dir(obj) or key in obj.__slots__:
+            setattr(obj, key, value)
+            return None
+    raise TypeError("can't add new attribute")
