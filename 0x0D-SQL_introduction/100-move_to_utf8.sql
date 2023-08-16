7f17b29daf06b05394e52241543abb5c1034ce2f
10thcode <attabadukwabena@gmail.com>
2023-08-16 02:00:37 +0000

    alter a database

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- converts hbtn_0c_0 database to UTF8
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 ALTER TABLE first_table CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE first_table ALTER COLUMN name CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
