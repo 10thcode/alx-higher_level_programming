9a67d9a2a52cc3ab5bb4a7259f7c0e0dc432db9d
10thcode <attabadukwabena@gmail.com>
2023-09-12 23:20:54 +0000

    Add 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let row = '';
+
+    for (let i = 0; i < this.width; i += 1) {
+      row = `${row}X`;
+    }
+
+    for (let i = 0; i < this.height; i += 1) {
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
