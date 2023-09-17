d9524a480de3cf861013928b78eb8f90475e9788
10thcode <attabadukwabena@gmail.com>
2023-09-17 23:40:18 +0000

    Fix pycodestyle error

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,7 +5,7 @@ letter a from the database hbtn_0e_6_usa
 '''
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.orm import sessionmaker
 import sys
 
 
