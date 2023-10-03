0dec86c3b549e0e5ab07c54fadd494eff0ee5987
10thcode <attabadukwabena@gmail.com>
2023-10-03 18:56:25 +0000

    Add 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Sends a request to a URL and displays
+the body response
+'''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
