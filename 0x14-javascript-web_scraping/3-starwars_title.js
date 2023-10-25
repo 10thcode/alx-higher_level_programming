026b5f748eeced8ba936565f68de9c2b7f0c3af1
10thcode <attabadukwabena@gmail.com>
2023-10-25 02:12:39 +0000

    Add 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const id = process.argv[2];
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const result = body.json();
+    console.log(result.title);
+  }
+});
