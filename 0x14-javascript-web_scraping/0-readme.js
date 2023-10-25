bb5763d4f84ce5f41483417d4f3e0814b09224d8
10thcode <attabadukwabena@gmail.com>
2023-10-25 01:44:27 +0000

    Add 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const file = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
