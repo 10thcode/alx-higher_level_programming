0bfaebce02004680fbc27d36ffd23591df3a1914
10thcode <attabadukwabena@gmail.com>
2023-11-02 21:23:42 +0000

    Add 100-script.js
    a JavaScript script that updates the text color of the <header> element

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+/**
+ * updates the text color of the <header> element
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
