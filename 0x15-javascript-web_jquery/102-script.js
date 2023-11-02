983b4db8aba86ec21782a149f7747923a1a1b07c
10thcode <attabadukwabena@gmail.com>
2023-11-02 21:56:02 +0000

    Add 102-script.js
    a JavaScript script that fetches and prints how to say “Hello” depending on the language

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,12 @@
+/**
+ * fetches and prints how to say “Hello” depending on the language
+ */
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    const lang = $('INPUT#language_code').val();
+    const url = `https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+    $.getJSON(url, function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
