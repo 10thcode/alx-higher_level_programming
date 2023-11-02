1739b1c128dfe0a5931e16ba0db150062c1e7000
10thcode <attabadukwabena@gmail.com>
2023-11-02 20:59:13 +0000

    Add 7-script.js
    a JavaScript script that fetches the character a from a URL

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+/**
+ * fetches a character name from a URL
+ */
+const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+$.getJSON(url, function (data) {
+  $('DIV#character').text(`${data.name}`);
+});
